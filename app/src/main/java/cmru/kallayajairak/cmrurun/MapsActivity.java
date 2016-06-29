30a1a33feb13d78b282a72c95bf8f87791ae5092
kallaya <ajkallaya@gmail.com>
2016-06-29 13:31:11 +0700

    setup cmru

--- app/src/main/java/cmru/kallayajairak/cmrurun/MapsActivity.java
+++ app/src/main/java/cmru/kallayajairak/cmrurun/MapsActivity.java
@@ -13,34 +13,28 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private double cmruLatADouble=18.807096, cmruLngADouble = 98.986530;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.my_layout);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
+    } // Main Method
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        //Setup to CMRU
+        LatLng latLng = new LatLng(cmruLatADouble, cmruLngADouble);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,16));
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-}
+
+
+    }//onMapReady
+} //Main Class
